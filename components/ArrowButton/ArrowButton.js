7b4ad6e5df0beeacbd0ae1f214012782c9ddd88e
Victor Paumier <victor.paumier@neuf.fr>
2022-09-30 13:46:49 +0200

    initialisation

---
+++ components/ArrowButton/ArrowButton.js
@@ -0,0 +1,10 @@
+import styles from './ArrowButton.module.scss';
+
+export default function ArrowButton() { 
+    return (
+  <div className={styles.button}>
+      <img src="images/next.png"></img>
+  </div>
+);
+}
+
---
+++ components/SideStripe/SideStripe.js
@@ -0,0 +1,19 @@
+import styles from "./sideStripe.module.scss";
+import Link from "next/link";
+
+export default function SideStripe() {
+  return (
+    <header className={styles.container}>
+      <Link href="/">
+        <h1>Arthur <br/> Paumier</h1>
+      </Link>
+      <p className={styles.container__description}>
+        This is a collection of my projects. In on hand you can find my
+        photogrpahies works. In the other hans there are my graphism works. a
+        click to insta will lead you to my instagram account . A click to
+        Architecture will leead you to my portfolio.
+      </p>
+      <p className={styles.container__copyright}>&copy; Victor Paumier, 2022</p>
+    </header>
+  );
+}
