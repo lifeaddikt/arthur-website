920c393511b1dc5bdba86236256d80e3e1faae85
Victor Paumier <75809044+lifeaddikt@users.noreply.github.com>
2022-11-06 20:40:57 +0100

    nextJS routing done

--- front/services/pictureService.js
+++ front/services/pictureService.js
@@ -1,8 +1,18 @@
 const pictureService = {
   baseUrl: process.env.NEXT_PUBLIC_APP_BACK_URL + 'wp/v2',
 
+  loadPictures : async () => {
+    return fetch(`${pictureService.baseUrl}/picture`)
+    .then(res => res.json())
+  },
+
   loadPicturesByCollection : async (slug) => {
     return fetch(`${pictureService.baseUrl}/picture?_embed&collection_slug=${slug}`)
+    .then(res => res.json())
+  },
+
+  loadPictureById : async (id) => {
+    return fetch(`${pictureService.baseUrl}/picture/{id}`)
     .then(res => res.json())
   },
 }
