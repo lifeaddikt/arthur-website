fbdf60122d0920443dfc7d4d670ad93f3f7b5623
Victor Paumier <victor.paumier@neuf.fr>
2025-02-28 12:29:09 +0100

    no migration file

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20250228_112701 from './20250228_112701';
-
-export const migrations = [
-  {
-    up: migration_20250228_112701.up,
-    down: migration_20250228_112701.down,
-    name: '20250228_112701'
-  },
-];
