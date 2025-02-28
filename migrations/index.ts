57b12754c64535fa47ef9cb4d24a51c2d41fb8cc
Victor Paumier <victor.paumier@neuf.fr>
2025-02-28 12:27:41 +0100

    new migration folder

--- migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20241220_140646_initial from './20241220_140646_initial';
-
-export const migrations = [
-  {
-    up: migration_20241220_140646_initial.up,
-    down: migration_20241220_140646_initial.down,
-    name: '20241220_140646_initial'
-  },
-];
--- src/migrations/20250228_112701.ts
+++ src/migrations/20250228_112701.ts
@@ -28,7 +28,52 @@ export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   	"width" numeric,
   	"height" numeric,
   	"focal_x" numeric,
-  	"focal_y" numeric
+  	"focal_y" numeric,
+  	"sizes_thumbnail_url" varchar,
+  	"sizes_thumbnail_width" numeric,
+  	"sizes_thumbnail_height" numeric,
+  	"sizes_thumbnail_mime_type" varchar,
+  	"sizes_thumbnail_filesize" numeric,
+  	"sizes_thumbnail_filename" varchar,
+  	"sizes_blur_url" varchar,
+  	"sizes_blur_width" numeric,
+  	"sizes_blur_height" numeric,
+  	"sizes_blur_mime_type" varchar,
+  	"sizes_blur_filesize" numeric,
+  	"sizes_blur_filename" varchar
+  );
+  
+  CREATE TABLE IF NOT EXISTS "photographies_collection" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"name" varchar NOT NULL,
+  	"slug" varchar NOT NULL,
+  	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL
+  );
+  
+  CREATE TABLE IF NOT EXISTS "photography" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"place" varchar NOT NULL,
+  	"date" varchar NOT NULL,
+  	"picture_id" integer NOT NULL,
+  	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL
+  );
+  
+  CREATE TABLE IF NOT EXISTS "photography_rels" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"order" integer,
+  	"parent_id" integer NOT NULL,
+  	"path" varchar NOT NULL,
+  	"photographies_collection_id" integer,
+  	"series_id" integer
+  );
+  
+  CREATE TABLE IF NOT EXISTS "series" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"name" varchar NOT NULL,
+  	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL
   );
   
   CREATE TABLE IF NOT EXISTS "payload_locked_documents" (
@@ -44,7 +89,10 @@ export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   	"parent_id" integer NOT NULL,
   	"path" varchar NOT NULL,
   	"users_id" integer,
-  	"media_id" integer
+  	"media_id" integer,
+  	"photographies_collection_id" integer,
+  	"photography_id" integer,
+  	"series_id" integer
   );
   
   CREATE TABLE IF NOT EXISTS "payload_preferences" (
@@ -72,6 +120,30 @@ export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   );
   
   DO $$ BEGIN
+   ALTER TABLE "photography" ADD CONSTRAINT "photography_picture_id_media_id_fk" FOREIGN KEY ("picture_id") REFERENCES "public"."media"("id") ON DELETE set null ON UPDATE no action;
+  EXCEPTION
+   WHEN duplicate_object THEN null;
+  END $$;
+  
+  DO $$ BEGIN
+   ALTER TABLE "photography_rels" ADD CONSTRAINT "photography_rels_parent_fk" FOREIGN KEY ("parent_id") REFERENCES "public"."photography"("id") ON DELETE cascade ON UPDATE no action;
+  EXCEPTION
+   WHEN duplicate_object THEN null;
+  END $$;
+  
+  DO $$ BEGIN
+   ALTER TABLE "photography_rels" ADD CONSTRAINT "photography_rels_photographies_collection_fk" FOREIGN KEY ("photographies_collection_id") REFERENCES "public"."photographies_collection"("id") ON DELETE cascade ON UPDATE no action;
+  EXCEPTION
+   WHEN duplicate_object THEN null;
+  END $$;
+  
+  DO $$ BEGIN
+   ALTER TABLE "photography_rels" ADD CONSTRAINT "photography_rels_series_fk" FOREIGN KEY ("series_id") REFERENCES "public"."series"("id") ON DELETE cascade ON UPDATE no action;
+  EXCEPTION
+   WHEN duplicate_object THEN null;
+  END $$;
+  
+  DO $$ BEGIN
    ALTER TABLE "payload_locked_documents_rels" ADD CONSTRAINT "payload_locked_documents_rels_parent_fk" FOREIGN KEY ("parent_id") REFERENCES "public"."payload_locked_documents"("id") ON DELETE cascade ON UPDATE no action;
   EXCEPTION
    WHEN duplicate_object THEN null;
@@ -85,6 +157,24 @@ export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   
   DO $$ BEGIN
    ALTER TABLE "payload_locked_documents_rels" ADD CONSTRAINT "payload_locked_documents_rels_media_fk" FOREIGN KEY ("media_id") REFERENCES "public"."media"("id") ON DELETE cascade ON UPDATE no action;
+  EXCEPTION
+   WHEN duplicate_object THEN null;
+  END $$;
+  
+  DO $$ BEGIN
+   ALTER TABLE "payload_locked_documents_rels" ADD CONSTRAINT "payload_locked_documents_rels_photographies_collection_fk" FOREIGN KEY ("photographies_collection_id") REFERENCES "public"."photographies_collection"("id") ON DELETE cascade ON UPDATE no action;
+  EXCEPTION
+   WHEN duplicate_object THEN null;
+  END $$;
+  
+  DO $$ BEGIN
+   ALTER TABLE "payload_locked_documents_rels" ADD CONSTRAINT "payload_locked_documents_rels_photography_fk" FOREIGN KEY ("photography_id") REFERENCES "public"."photography"("id") ON DELETE cascade ON UPDATE no action;
+  EXCEPTION
+   WHEN duplicate_object THEN null;
+  END $$;
+  
+  DO $$ BEGIN
+   ALTER TABLE "payload_locked_documents_rels" ADD CONSTRAINT "payload_locked_documents_rels_series_fk" FOREIGN KEY ("series_id") REFERENCES "public"."series"("id") ON DELETE cascade ON UPDATE no action;
   EXCEPTION
    WHEN duplicate_object THEN null;
   END $$;
@@ -107,6 +197,20 @@ export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   CREATE INDEX IF NOT EXISTS "media_updated_at_idx" ON "media" USING btree ("updated_at");
   CREATE INDEX IF NOT EXISTS "media_created_at_idx" ON "media" USING btree ("created_at");
   CREATE UNIQUE INDEX IF NOT EXISTS "media_filename_idx" ON "media" USING btree ("filename");
+  CREATE INDEX IF NOT EXISTS "media_sizes_thumbnail_sizes_thumbnail_filename_idx" ON "media" USING btree ("sizes_thumbnail_filename");
+  CREATE INDEX IF NOT EXISTS "media_sizes_blur_sizes_blur_filename_idx" ON "media" USING btree ("sizes_blur_filename");
+  CREATE INDEX IF NOT EXISTS "photographies_collection_updated_at_idx" ON "photographies_collection" USING btree ("updated_at");
+  CREATE INDEX IF NOT EXISTS "photographies_collection_created_at_idx" ON "photographies_collection" USING btree ("created_at");
+  CREATE INDEX IF NOT EXISTS "photography_picture_idx" ON "photography" USING btree ("picture_id");
+  CREATE INDEX IF NOT EXISTS "photography_updated_at_idx" ON "photography" USING btree ("updated_at");
+  CREATE INDEX IF NOT EXISTS "photography_created_at_idx" ON "photography" USING btree ("created_at");
+  CREATE INDEX IF NOT EXISTS "photography_rels_order_idx" ON "photography_rels" USING btree ("order");
+  CREATE INDEX IF NOT EXISTS "photography_rels_parent_idx" ON "photography_rels" USING btree ("parent_id");
+  CREATE INDEX IF NOT EXISTS "photography_rels_path_idx" ON "photography_rels" USING btree ("path");
+  CREATE INDEX IF NOT EXISTS "photography_rels_photographies_collection_id_idx" ON "photography_rels" USING btree ("photographies_collection_id");
+  CREATE INDEX IF NOT EXISTS "photography_rels_series_id_idx" ON "photography_rels" USING btree ("series_id");
+  CREATE INDEX IF NOT EXISTS "series_updated_at_idx" ON "series" USING btree ("updated_at");
+  CREATE INDEX IF NOT EXISTS "series_created_at_idx" ON "series" USING btree ("created_at");
   CREATE INDEX IF NOT EXISTS "payload_locked_documents_global_slug_idx" ON "payload_locked_documents" USING btree ("global_slug");
   CREATE INDEX IF NOT EXISTS "payload_locked_documents_updated_at_idx" ON "payload_locked_documents" USING btree ("updated_at");
   CREATE INDEX IF NOT EXISTS "payload_locked_documents_created_at_idx" ON "payload_locked_documents" USING btree ("created_at");
@@ -115,6 +219,9 @@ export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   CREATE INDEX IF NOT EXISTS "payload_locked_documents_rels_path_idx" ON "payload_locked_documents_rels" USING btree ("path");
   CREATE INDEX IF NOT EXISTS "payload_locked_documents_rels_users_id_idx" ON "payload_locked_documents_rels" USING btree ("users_id");
   CREATE INDEX IF NOT EXISTS "payload_locked_documents_rels_media_id_idx" ON "payload_locked_documents_rels" USING btree ("media_id");
+  CREATE INDEX IF NOT EXISTS "payload_locked_documents_rels_photographies_collection_id_idx" ON "payload_locked_documents_rels" USING btree ("photographies_collection_id");
+  CREATE INDEX IF NOT EXISTS "payload_locked_documents_rels_photography_id_idx" ON "payload_locked_documents_rels" USING btree ("photography_id");
+  CREATE INDEX IF NOT EXISTS "payload_locked_documents_rels_series_id_idx" ON "payload_locked_documents_rels" USING btree ("series_id");
   CREATE INDEX IF NOT EXISTS "payload_preferences_key_idx" ON "payload_preferences" USING btree ("key");
   CREATE INDEX IF NOT EXISTS "payload_preferences_updated_at_idx" ON "payload_preferences" USING btree ("updated_at");
   CREATE INDEX IF NOT EXISTS "payload_preferences_created_at_idx" ON "payload_preferences" USING btree ("created_at");
@@ -130,6 +237,10 @@ export async function down({ db, payload, req }: MigrateDownArgs): Promise<void>
   await db.execute(sql`
    DROP TABLE "users" CASCADE;
   DROP TABLE "media" CASCADE;
+  DROP TABLE "photographies_collection" CASCADE;
+  DROP TABLE "photography" CASCADE;
+  DROP TABLE "photography_rels" CASCADE;
+  DROP TABLE "series" CASCADE;
   DROP TABLE "payload_locked_documents" CASCADE;
   DROP TABLE "payload_locked_documents_rels" CASCADE;
   DROP TABLE "payload_preferences" CASCADE;
