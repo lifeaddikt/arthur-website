dc788ce250326b08ec1c14be7a330e17241f7ec1
Victor Paumier <victor.paumier@neuf.fr>
2025-03-07 12:15:35 +0100

    test blur static url

--- components/grid/MansonryGrid.tsx
+++ components/grid/MansonryGrid.tsx
@@ -24,6 +24,7 @@ const MasonryGrid = ({ photos, collection }: MasonryGridProps) => {
       className='my-masonry-grid mt-8'
       columnClassName='my-masonry-grid_column'>
       {photos.map(({ id, picture }, index) => {
+        console.log(picture)
         if (typeof picture === 'number' || !picture?.url) return null
         return (
           <motion.div
@@ -38,8 +39,8 @@ const MasonryGrid = ({ photos, collection }: MasonryGridProps) => {
               <Image
                 className='dark:invert'
                 draggable={false}
-                priority={index < 7}
-                loading={index < 7 ? 'eager' : 'lazy'}
+                priority={index < 4}
+                loading={index < 4 ? 'eager' : 'lazy'}
                 src={picture.url}
                 alt={picture.alt || 'Photo'}
                 width={Number(picture.width) || 0}
@@ -47,7 +48,8 @@ const MasonryGrid = ({ photos, collection }: MasonryGridProps) => {
                 sizes="(max-width: 768px) 100vw, 33vw"
                 placeholder='blur'
                 style={{ viewTransitionName: `photo-${id}` }}
-                blurDataURL={picture.sizes?.blur?.url || ''}
+                blurDataURL={'data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/4gHYSUNDX1BST0ZJTEUAAQEAAAHIAAAAAAQwAABtbnRyUkdCIFhZWiAH4AABAAEAAAAAAABhY3NwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQAA9tYAAQAAAADTLQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAlkZXNjAAAA8AAAACRyWFlaAAABFAAAABRnWFlaAAABKAAAABRiWFlaAAABPAAAABR3dHB0AAABUAAAABRyVFJDAAABZAAAAChnVFJDAAABZAAAAChiVFJDAAABZAAAAChjcHJ0AAABjAAAADxtbHVjAAAAAAAAAAEAAAAMZW5VUwAAAAgAAAAcAHMAUgBHAEJYWVogAAAAAAAAb6IAADj1AAADkFhZWiAAAAAAAABimQAAt4UAABjaWFlaIAAAAAAAACSgAAAPhAAAts9YWVogAAAAAAAA9tYAAQAAAADTLXBhcmEAAAAAAAQAAAACZmYAAPKnAAANWQAAE9AAAApbAAAAAAAAAABtbHVjAAAAAAAAAAEAAAAMZW5VUwAAACAAAAAcAEcAbwBvAGcAbABlACAASQBuAGMALgAgADIAMAAxADb/2wBDABQODxIPDRQSEBIXFRQdHx4eHRoaHSQtJSEkMjU0LS0yMi4qIiUvKTI+QjIyNjo6Pj4+LzI4RkU/QjU+Pj7/2wBDAR'}
+                quality={80}
               />
             </Link>
           </motion.div>
