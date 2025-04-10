4079413c2b8109f9f3c6acd29391ac9b9a97f2f0
Victor Paumier <victor.paumier@neuf.fr>
2025-04-10 16:21:35 +0200

    remove console

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -17,9 +17,9 @@ const ThemeToggle = () => {
     return (
       <button
         className='flex items-center justify-center w-12 h-6 rounded-full bg-gray-200 dark:bg-gray-700'
-        aria-label="Theme toggle"
+        aria-label='Theme toggle'
       >
-        <div className="w-5 h-5 rounded-full bg-white shadow-sm" />
+        <div className='w-5 h-5 rounded-full bg-white shadow-sm' />
       </button>
     )
   }
@@ -28,16 +28,14 @@ const ThemeToggle = () => {
     <button
       onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
       className='flex items-center justify-center w-12 h-6 rounded-full focus:outline-none focus:ring-1 focus:ring-offset-1 transition-colors duration-200 bg-gray-200 dark:bg-gray-700'
-      aria-label={resolvedTheme === 'dark' ? 'Use light mode' : 'Use dark mode'}>
+      aria-label={resolvedTheme === 'dark' ? 'Use light mode' : 'Use dark mode'}
+    >
       <div
         className={`w-5 h-5 rounded-full bg-white shadow-sm transform transition-transform duration-200 flex items-center justify-center ${
           resolvedTheme === 'dark' ? 'translate-x-2' : '-translate-x-2'
-        }`}>
-        {resolvedTheme === 'dark' ? (
-          <Moon />
-        ) : (
-          <Sun />
-        )}
+        }`}
+      >
+        {resolvedTheme === 'dark' ? <Moon /> : <Sun />}
       </div>
     </button>
   )
