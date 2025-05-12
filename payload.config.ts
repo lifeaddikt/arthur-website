3bd88da1a8a30601a366cffe10bc7beec7a681de
Victor Paumier <victor.paumier@neuf.fr>
2025-05-12 15:18:36 +0200

    fix build error

--- payload.config.ts
+++ payload.config.ts
@@ -14,22 +14,6 @@ import { Series } from './collections/Series'
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
-interface S3Config {
-  collections: {
-    media: boolean
-  }
-  bucket: string
-  config: {
-    credentials: {
-      accessKeyId: string
-      secretAccessKey: string
-    }
-    region: string
-  }
-  clientUploads: boolean
-  cdnUrl: string
-}
-
 export default buildConfig({
   admin: {
     user: Users.slug,
@@ -71,6 +55,6 @@ export default buildConfig({
       },
       clientUploads: true,
       cdnUrl: process.env.S3_CDN_URL || '',
-    } as S3Config),
+    } as any),
   ],
 })
